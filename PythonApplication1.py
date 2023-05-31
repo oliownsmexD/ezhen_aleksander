1148eb51784c139b1408c81be30c907ec22ca426
oliownsmexD <135156225+oliownsmexD@users.noreply.github.com>
2023-05-31 20:54:37 +0300

    lab 3

---
+++ PythonApplication1.py
@@ -0,0 +1,17 @@
+print("Enter your numbers with a space")
+
+numlist = list(map(int, input().split()))
+
+
+
+
+
+
+
+for a in numlist:
+
+    for b in numlist:
+
+        if a + b == 10:
+
+            print(a , "+" ,b)
